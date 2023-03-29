cfd87c06eb59ee3ea7420e152bbc759bd86a16a3
Enrique Prieto <enrique.neto@safetydocs.com.br>
2023-03-28 21:25:44 -0300

    chore: change navabar config

---
+++ src/config/navbar.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: 'Prieto Blog',
+    logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+    },
+    items: [
+        {
+            to: '/', 
+            label: 'Home', 
+            position: 'left'
+        },
+        {
+            type: 'localeDropdown',
+            position: 'right', 
+        }
+    ]
+}
